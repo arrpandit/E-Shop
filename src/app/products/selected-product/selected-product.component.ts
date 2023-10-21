dc072a2bdee431868c7ccf0265cbea82b6a6dd21
Alok Ranjan Pandit <alokranjanpandit005@gmail.com>
2023-10-22 00:20:16 +0530

    select product UI

--- src/app/products/selected-product/selected-product.component.ts
+++ src/app/products/selected-product/selected-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from 'src/app/services/product/products.service';
+import { products } from 'src/data-type';
 
 @Component({
   selector: 'app-selected-product',
@@ -7,13 +9,24 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./selected-product.component.css']
 })
 export class SelectedProductComponent implements OnInit {
+  data:undefined|products[]=[];
 
-  constructor(private route : ActivatedRoute) { }
+  constructor(private route : ActivatedRoute, private productService:ProductsService) { }
 
   ngOnInit(): void {
     let productId = this.route.snapshot.paramMap.get('id')
     // let productid = this.route.snapshot.paramMap.get('id');
-    console.log("product is---------",productId);
+    if(productId){
+      this.productService.getProductbyID(productId).subscribe((res:any)=>{
+        if(res){
+          // console.log("res--------------",res);
+          this.data?.push(res);
+        }
+        
+      })
+    }
+    
+    console.log("product is---------",this.data);
     
   }
 
